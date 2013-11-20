7da8462e59362020e9b770f21b903fcd40bd19a6
KnightMB <knightmb@timekoin.org>
2013-11-20 14:04:08 -0600

    - Some windows systems still have timeout issues when clock settings are updated, up the timeout from 99 to 300 seconds

--- plugins/Official/Server/AutoTransfer/autotransfer.php
+++ plugins/Official/Server/AutoTransfer/autotransfer.php
@@ -34,7 +34,7 @@ if($_SESSION["valid_login"] == FALSE)
 
 	while(1) // Begin Infinite Loop :)
 	{
-		set_time_limit(99); // Reset Timeout
+		set_time_limit(300); // Reset Timeout
 
 		// Are we to remain active?
 		$timekoin_active = mysql_result(mysql_query("SELECT * FROM `main_loop_status` WHERE `field_name` = 'main_heartbeat_active' LIMIT 1"),0,"field_data");
