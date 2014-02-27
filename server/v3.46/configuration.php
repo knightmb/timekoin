516486dd809e4999bf4f6b71100734f01d8bc1a8
KnightMB <knightmb@timekoin.org>
2014-02-26 23:54:32 -0600

    Add v3.46 Files for future updates or development

---
+++ server/v3.46/configuration.php
@@ -0,0 +1,7 @@
+<?PHP
+define("MYSQL_IP","localhost"); //database connection IP
+define("MYSQL_USERNAME","tkuser"); //database username
+define("MYSQL_PASSWORD","12345"); //database password
+define("MYSQL_DATABASE","timekoin"); //database everything is stored in 
+
+?>
