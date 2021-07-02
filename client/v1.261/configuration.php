53b1951f79fce7d81a5c39b61b37971c4398eca9
KnightMB <knightmb@timekoin.net>
2021-07-02 17:17:44 -0500

    - Minor update for Client v1.26 -> 1.261
    - Allow very large public keys for Easy Key lookup, was capped to 500 characters before, now capped at 4096 characters

---
+++ client/v1.261/configuration.php
@@ -0,0 +1,7 @@
+<?PHP
+define("MYSQL_IP","localhost"); //database connection IP
+define("MYSQL_USERNAME","tk_client"); //database username
+define("MYSQL_PASSWORD","12345"); //database password
+define("MYSQL_DATABASE","tk_client_db"); //database everything is stored in 
+
+?>
